2e25336c08c72ad4b3d3dcb3da7bf66947d382dc
lthomp079 <163169113+lthomp079@users.noreply.github.com>
2024-03-12 21:02:21 -0400

    Update Setup.sh
    works with new sc

--- Linux/Setup.sh
+++ Linux/Setup.sh
@@ -1,8 +1,8 @@
-chmod u+x MathSuite_v1.01.linux
+chmod u+x MathSuite_v1.02.linux
 read -p "Executable has been made!"
 read -p "Now run MathSuite.sh in the terminal with bash MathSuite.sh!"
 read -p "Any key to exit...." -t 2
 read -p " " -t 0
 echo "rm -rf Setup.sh" >> MathSuite.sh
-echo "./MathSuite_v1.01.linux" >> MathSuite.sh
+echo "./MathSuite_v1.02.linux" >> MathSuite.sh
 chmod u+x MathSuite.sh
